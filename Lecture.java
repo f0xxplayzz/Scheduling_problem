d2b3d474429c1ae7f942afa3961538615fbf4f20
f0xxplayzz <f0xxplayzz@gmail.com>
2022-04-23 19:50:04 +0200

    java implementation

---
+++ Lecture.java
@@ -0,0 +1,18 @@
+public class Lecture {
+    int i;
+    int room;
+    int time;
+    int day;
+
+    public Lecture(int i, int room, int time, int day) {
+        this.i = i;
+        this.room = room;
+        this.time = time;
+        this.day = day;
+    }
+
+    public void print() {
+        String val = "lecture: " + i + "| room: " + room + "| time: " + time + "| day: " + day;
+        System.out.println(val);
+    }
+}
