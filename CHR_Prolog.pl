8e7884edf674848127754c291faa593773f2544f
f0xxplayzz <67318323+f0xxplayzz@users.noreply.github.com>
2022-04-25 16:30:56 +0200

    rollback

--- CHR_Prolog.pl
+++ CHR_Prolog.pl
@@ -12,65 +12,65 @@ nonOverlapping([H|_],[H2|_],_) <=> H #= H2 | false.
 nonOverlapping([H|T],[],R) <=> nonOverlapping(T,[H|R],[]).
 nonOverlapping([],_,_) <=> true.
 
-:-chr_constraint minOneLecturePerDay(?any,?int).
-:-chr_constraint minOneLecturePerDay_(?any,?int).
-:-chr_constraint lectures(?any,?int).
-:-chr_constraint lectureDay(?int,?int).
-:-chr_constraint repeat(?any,?int).
+:-chr_constraint minOneLecturePerDay(?any).
+:-chr_constraint minOneLecturePerDay_(?any).
+:-chr_constraint lectures(?any).
+:-chr_constraint lectureDay(?int).
+:-chr_constraint repeat(?any).
 
 %minOneLecturePerDay([],[],[]),lectureDay(A) <=> A #= 5 | true.
 
-minOneLecturePerDay(X,ID) <=> minOneLecturePerDay_(X,ID), repeat(0,ID).
+minOneLecturePerDay(X) <=> minOneLecturePerDay_(X), repeat(0).
 
-minOneLecturePerDay_([H|T],ID),repeat(A,ID1) <=> ID #= ID1 | New #= (H-1) // 3, A2 #= A +1, lectureDay(New,ID),minOneLecturePerDay_(T,ID),repeat(A2,ID).
+minOneLecturePerDay_([H|T]),repeat(A) <=> true | New #= (H-1) // 3, A2 #= A +1, lectureDay(New),minOneLecturePerDay_(T),repeat(A2).
 
-minOneLecturePerDay_([],ID) <=> lectures([0,0,0,0,0],ID).
+minOneLecturePerDay_([]) <=> lectures([0,0,0,0,0]).
 
-lectures([A,B,C,D,E],ID), lectureDay(Day,ID2),repeat(X,ID3) <=> Day #= 0, ID #=ID2 , ID2 #= ID3| NewA #= A +1 ,lectures([NewA,B,C,D,E],ID),A2 #= X -1, repeat(A2,ID).
-lectures([A,B,C,D,E],ID), lectureDay(Day,ID2),repeat(X,ID3) <=> Day #= 1, ID #=ID2 , ID2 #= ID3| NewB #= B +1 ,lectures([A,NewB,C,D,E],ID),A2 #= X -1, repeat(A2,ID).
-lectures([A,B,C,D,E],ID), lectureDay(Day,ID2),repeat(X,ID3) <=> Day #= 2, ID #=ID2 , ID2 #= ID3| NewC #= C +1 ,lectures([A,B,NewC,D,E],ID),A2 #= X -1, repeat(A2,ID).
-lectures([A,B,C,D,E],ID), lectureDay(Day,ID2),repeat(X,ID3) <=> Day #= 3, ID #=ID2 , ID2 #= ID3| NewD #= D +1 ,lectures([A,B,C,NewD,E],ID),A2 #= X -1, repeat(A2,ID).
-lectures([A,B,C,D,E],ID), lectureDay(Day,ID2),repeat(X,ID3) <=> Day #= 4, ID #=ID2 , ID2 #= ID3| NewE #= E +1 ,lectures([A,B,C,D,NewE],ID),A2 #= X -1, repeat(A2,ID).
+lectures([A,B,C,D,E]), lectureDay(Day),repeat(X) <=> Day #= 0, true| NewA #= A +1 ,lectures([NewA,B,C,D,E]),A2 #= X -1, repeat().
+lectures([A,B,C,D,E]), lectureDay(Day),repeat(X) <=> Day #= 1| NewB #= B +1 ,lectures([A,NewB,C,D,E]),A2 #= X -1, repeat(A2).
+lectures([A,B,C,D,E]), lectureDay(Day),repeat(X) <=> Day #= 2| NewC #= C +1 ,lectures([A,B,NewC,D,E]),A2 #= X -1, repeat(A2).
+lectures([A,B,C,D,E]), lectureDay(Day),repeat(X) <=> Day #= 3| NewD #= D +1 ,lectures([A,B,C,NewD,E]),A2 #= X -1, repeat(A2).
+lectures([A,B,C,D,E]), lectureDay(Day),repeat(X) <=> Day #= 4| NewE #= E +1 ,lectures([A,B,C,D,NewE]),A2 #= X -1, repeat(A2).
 
-lectures([A|_],ID),repeat(0,ID2) <=> A #= 0, ID #= ID2 | false.
-lectures([A|T],ID),repeat(0,ID2) <=> A #\= 0, ID #= ID2 | lectures(T,ID),repeat(0,ID).
-lectures([],ID),repeat(0,ID2) <=> ID #= ID2 | true.
+lectures([A|_]),repeat(0) <=> A #= 0| false.
+lectures([A|T]),repeat(0) <=> A #\= 0| lectures(T,ID),repeat(0,ID).
+lectures([]),repeat(0) <=> true.
 
-:-chr_constraint maxTwoLecturesPerDay(?any,?int).
-:-chr_constraint maxTwoLecturesPerDay_(?any,?int).
-:-chr_constraint lectureDayDoz(?int,?int).
-:-chr_constraint lecturesDoz(?any,?int).
-:-chr_constraint repeatDoz(?any,?int).
+:-chr_constraint maxTwoLecturesPerDay(?any).
+:-chr_constraint maxTwoLecturesPerDay_(?any).
+:-chr_constraint lectureDayDoz(?int).
+:-chr_constraint lecturesDoz(?any).
+:-chr_constraint repeatDoz(?any).
 
-maxTwoLecturesPerDay(X,ID) <=> maxTwoLecturesPerDay_(X,ID), repeatDoz(0,ID).
+maxTwoLecturesPerDay(X) <=> maxTwoLecturesPerDay_(X), repeatDoz(0).
 
-maxTwoLecturesPerDay_([H|T],ID),repeatDoz(A,ID2) <=> ID #= ID2 | New #= (H-1) // 3, A2 #= A +1, lectureDayDoz(New,ID),maxTwoLecturesPerDay_(T,ID),repeatDoz(A2,ID).
+maxTwoLecturesPerDay_([H|T]),repeatDoz(A) <=> true| New #= (H-1) // 3, A2 #= A +1, lectureDayDoz(New),maxTwoLecturesPerDay_(T),repeatDoz(A2).
 
-maxTwoLecturesPerDay_([],ID) <=> lecturesDoz([0,0,0,0,0],ID).
+maxTwoLecturesPerDay_([]) <=> lecturesDoz([0,0,0,0,0]).
 
-lecturesDoz([A,B,C,D,E],ID), lectureDayDoz(Day,ID2),repeatDoz(X,ID3) <=> Day #= 0, ID#= ID2, ID2 #= ID3 | NewA #= A +1 ,lecturesDoz([NewA,B,C,D,E],ID),A2 #= X -1, repeatDoz(A2,ID).
-lecturesDoz([A,B,C,D,E],ID), lectureDayDoz(Day,ID2),repeatDoz(X,ID3) <=> Day #= 1, ID#= ID2, ID2 #= ID3 | NewB #= B +1 ,lecturesDoz([A,NewB,C,D,E],ID),A2 #= X -1, repeatDoz(A2,ID).
-lecturesDoz([A,B,C,D,E],ID), lectureDayDoz(Day,ID2),repeatDoz(X,ID3) <=> Day #= 2, ID#= ID2, ID2 #= ID3 | NewC #= C +1 ,lecturesDoz([A,B,NewC,D,E],ID),A2 #= X -1, repeatDoz(A2,ID).
-lecturesDoz([A,B,C,D,E],ID), lectureDayDoz(Day,ID2),repeatDoz(X,ID3) <=> Day #= 3, ID#= ID2, ID2 #= ID3 | NewD #= D +1 ,lecturesDoz([A,B,C,NewD,E],ID),A2 #= X -1, repeatDoz(A2,ID).
-lecturesDoz([A,B,C,D,E],ID), lectureDayDoz(Day,ID2),repeatDoz(X,ID3) <=> Day #= 4, ID#= ID2, ID2 #= ID3 | NewE #= E +1 ,lecturesDoz([A,B,C,D,NewE],ID),A2 #= X -1, repeatDoz(A2,ID).
+lecturesDoz([A,B,C,D,E]), lectureDayDoz(Day),repeatDoz(X) <=> Day #= 0| NewA #= A +1 ,lecturesDoz([NewA,B,C,D,E]),A2 #= X -1, repeatDoz(A2).
+lecturesDoz([A,B,C,D,E]), lectureDayDoz(Day),repeatDoz(X) <=> Day #= 1| NewB #= B +1 ,lecturesDoz([A,NewB,C,D,E]),A2 #= X -1, repeatDoz(A2).
+lecturesDoz([A,B,C,D,E]), lectureDayDoz(Day),repeatDoz(X) <=> Day #= 2| NewC #= C +1 ,lecturesDoz([A,B,NewC,D,E]),A2 #= X -1, repeatDoz(A2).
+lecturesDoz([A,B,C,D,E]), lectureDayDoz(Day),repeatDoz(X) <=> Day #= 3| NewD #= D +1 ,lecturesDoz([A,B,C,NewD,E]),A2 #= X -1, repeatDoz(A2).
+lecturesDoz([A,B,C,D,E]), lectureDayDoz(Day),repeatDoz(X) <=> Day #= 4| NewE #= E +1 ,lecturesDoz([A,B,C,D,NewE]),A2 #= X -1, repeatDoz(A2).
 
-lecturesDoz([A|_],ID),repeatDoz(0,ID2) <=> A #> 2, ID #= ID2 | false.
-lecturesDoz([A|T],ID),repeatDoz(0,ID2) <=> A #=< 2, ID #= ID2 | lecturesDoz(T,ID),repeatDoz(0,ID).
-lecturesDoz([],ID),repeatDoz(0,ID2) <=> ID #= ID2 | true.
+lecturesDoz([A|_]),repeatDoz(0) <=> A #> 2| false.
+lecturesDoz([A|T]),repeatDoz(0) <=> A #=< 2 | lecturesDoz(T),repeatDoz(0).
+lecturesDoz([]),repeatDoz(0) <=> true.
 
-:-chr_constraint nonOverlappingRooms(?any,?any,?int).
-:-chr_constraint lectureUnit(?int,?int,?int).
+:-chr_constraint nonOverlappingRooms(?any,?any).
+:-chr_constraint lectureUnit(?int,?int).
 
 
 
-nonOverlappingRooms([H|T],[H2|T2],ID) <=> lectureUnit(H,H2,ID),nonOverlappingRooms(T,T2,ID).
-nonOverlappingRooms([],[],_) <=> true.
-nonOverlappingRooms([],[_|_],_) ==> false.
-nonOverlappingRooms([_|_],[],_) ==> false.
+nonOverlappingRooms([H|T],[H2|T2]) <=> lectureUnit(H,H2),nonOverlappingRooms(T,T2).
+nonOverlappingRooms([],[]) <=> true.
+nonOverlappingRooms([],[_|_]) ==> false.
+nonOverlappingRooms([_|_],[]) ==> false.
 
-lectureUnit(A,B,ID),lectureUnit(C,D,ID2) <=> A #= C, B #= D, ID #= ID2 | false.
+lectureUnit(A,B),lectureUnit(C,D) <=> A #= C, B #= D| false.
 
-lectureUnit(A,B,ID),lectureUnit(C,D,ID) ==> A #\=C ; B #\= D | true.
+lectureUnit(A,B),lectureUnit(C,D) ==> A #\=C ; B #\= D | true.
 
 schedule(Subs,Rooms) :-
     Subs = [DB2,ITS,KI,LP,FP,ASWE,SV,DB1,TINF3,SWE,M2,P2,RA,BS,DT,TINF1,M1,P1,VLA,WE,BWL],
