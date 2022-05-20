3eaf6f07b7082b5b68816e80a8e93549fc91ecf8
f0xxplayzz <f0xxplayzz@gmail.com>
2022-05-20 14:52:53 +0200

    finished choco implementation

--- choco/src/main/java/de/dhbw/Scheduling.java
+++ choco/src/main/java/de/dhbw/Scheduling.java
@@ -1,31 +1,134 @@
 package de.dhbw;
 
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.variables.IntVar;
 
 public class Scheduling {
     public static void main(String[] args) {
         Model model = new Model("scheduling-dhbw");
 
+        IntVar[] rooms = new IntVar[21];
+        IntVar[] lectRoomComb = new IntVar[21];
+
         IntVar[] lectures = new IntVar[21];
-        for(int l = 0; l < 21; l++){
-            lectures[l] = model.intVar("L_"+l, 1, 15);
+        IntVar[] days = new IntVar[21];
+
+        int[] dayValues = {0,1,2,3,4};
+        IntVar[] studDayValues = new IntVar[5];
+        IntVar[] dozDayValues = new IntVar[5];
+
+        IntVar[] days2019 = new IntVar[7];
+        IntVar[] days2020 = new IntVar[7];
+        IntVar[] days2021 = new IntVar[7];
+
+        IntVar[] daysB = new IntVar[4];
+        IntVar[] daysC = new IntVar[3];
+        IntVar[] daysG = new IntVar[3];
+
+        for(int i = 0;i<studDayValues.length;i++){
+            studDayValues[i] = model.intVar(1,3);
+            dozDayValues[i] = model.intVar(0,2);
         }
 
+        for(int l = 0; l < 21; l++){
+            lectures[l] = model.intVar("L_"+l, 0, 14);
+            days[l] = model.intVar("D_"+l,0,4);
+            model.div(lectures[l],model.intVar(3),days[l]).post();
+            rooms[l] = model.intVar("Room_Lect_" + l,1,5);
+            IntVar temp = model.intVar(15,75);
+            model.times(rooms[l],15,temp).post();
+            lectRoomComb[l] = model.intVar(15,90);
+            model.arithm(lectRoomComb[l],"=",temp,"+",lectures[l]).post();
+        }
+
+
+        //Kurse
         for(int i = 0 ; i< 7;i++){
-            for(int j = i+1; i<7;i++){
-                model.arithm(lectures[i],"!=",lectures[j]);
+            days2019[i] = days[i];
+            for(int j = i+1; j<7;j++){
+                model.arithm(lectures[i],"!=",lectures[j]).post();
             }
         }
+
         for(int i = 7 ; i< 14;i++){
-            for(int j = i+1; i<14;i++){
-                model.arithm(lectures[i],"!=",lectures[j]);
+            days2020[i-7] = days[i];
+            for(int j = i+1; j<14;j++){
+                model.arithm(lectures[i],"!=",lectures[j]).post();
             }
         }
         for(int i = 14 ; i< 21;i++){
-            for(int j = i+1; i<21;i++){
-                model.arithm(lectures[i],"!=",lectures[j]);
+            days2021[i-14] = days[i];
+            for(int j = i+1; j<21;j++){
+                model.arithm(lectures[i],"!=",lectures[j]).post();
             }
+        }
+
+        //Dozierende
+        model.arithm(lectures[0],"!=",lectures[7]).post(); //A
+
+        //B
+        IntVar[] dozB = {lectures[2],lectures[3],lectures[5],lectures[15]};
+        for(int i = 0; i< dozB.length;i++){
+            daysB[i] = dozB[i];
+            for(int j = i+1;j<dozB.length;j++){
+                model.arithm(dozB[i],"!=",dozB[j]).post();
+            }
+        }
+
+        //C
+        IntVar[] dozC = {lectures[4],lectures[8],lectures[17]};
+        for(int i = 0; i< dozC.length;i++){
+            daysC[i] = dozC[i];
+            for(int j = i+1;j<dozC.length;j++){
+                model.arithm(dozC[i],"!=",dozC[j]).post();
+            }
+        }
+
+        //D
+        model.arithm(lectures[19],"!=",lectures[20]).post();
+
+        //E
+        model.arithm(lectures[9],"!=",lectures[11]).post();
+
+        //G
+        IntVar[] dozG = {lectures[12],lectures[13],lectures[14]};
+        for(int i = 0; i< dozG.length;i++){
+            daysG[i] = dozG[i];
+            for(int j = i+1;j<dozG.length;j++){
+                model.arithm(dozG[i],"!=",dozG[j]).post();
+            }
+        }
+
+        //H
+        model.arithm(lectures[1],"!=",lectures[6]).post();
+
+        //I
+        model.arithm(lectures[10],"!=",lectures[16]).post();
+
+        model.globalCardinality(days2019,dayValues,studDayValues,false).post();
+        model.globalCardinality(days2020,dayValues,studDayValues,false).post();
+        model.globalCardinality(days2021,dayValues,studDayValues,false).post();
+
+        model.globalCardinality(daysB,dayValues,dozDayValues,false).post();
+        model.globalCardinality(daysC,dayValues,dozDayValues,false).post();
+        model.globalCardinality(daysG,dayValues,dozDayValues,false).post();
+
+        model.allDifferent(lectRoomComb).post();
+
+        Solution solution = model.getSolver().findSolution();
+
+        if(solution != null){
+            for(IntVar i: lectures){
+                int temp = solution.getIntVal(i);
+                System.out.println(i.getName() + ":" + temp);
+            }
+            for(IntVar i: rooms){
+                int temp = solution.getIntVal(i);
+                System.out.println(i.getName() + ":" + temp);
+            }
+        } else {
+            System.out.println("problem");
         }
     }
 }
